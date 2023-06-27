4d9feb41510855459048d7019d2b8f33f00f13e2
Kylix Afonso <kylix@liqwid.finance>
2023-06-27 19:31:18 +0100

    add `pairWiseZipWith`

--- src/Data/Series.hs
+++ src/Data/Series.hs
@@ -12,66 +12,15 @@ module Data.Series (
   resampleSAH,
   bounds,
   isEmpty,
+  pairWiseZipWith,
 )
 where
 
-import Data.Foldable (for_)
 import Data.List (sortOn)
-import Data.STRef (modifySTRef, readSTRef)
-import Data.STRef.Strict (newSTRef)
-import Data.Series.Internal (DataPoint (..), Series (..), binarySearch, exact, inclusiveSlice)
-import Data.Series.TimeRange (TimeRange (TimeRange))
+import Data.Series.Internal (DataPoint (..), Series (..), binarySearch, emptySeries, exact, inclusiveSlice, isEmpty, merge, resampleSAH, pairWiseZipWith)
 import Data.Time (UTCTime)
-import Data.Vector (Vector)
 import Data.Vector qualified as Vector
-import Data.Vector.Mutable qualified as MVector
-
-findLargestSmallerThan ::
-  forall a.
-  UTCTime ->
-  Series a ->
-  Maybe (DataPoint a)
-findLargestSmallerThan t (Series xs) =
-  lastMaybe $
-    Vector.filter
-      (\(DataPoint t0 _) -> t0 <= t)
-      xs
-  where
-    lastMaybe :: forall b. Vector b -> Maybe b
-    lastMaybe xs' | Vector.null xs' = Nothing
-    lastMaybe xs' = Just $ Vector.last xs'
-
-{- | Create a new series with the times in the given vector, at each of the
-     times in the vector place the last registered value in the given series
-     before that time.
-
-     If a given time from the vector is before the first time in the series,
-     drop it.
-
-     Passing an empty vector or series will always result in an empty series.
--}
-resampleSAH :: forall a. Vector UTCTime -> Series a -> Series a
-resampleSAH _ xs | isEmpty xs = emptySeries
-resampleSAH ts xs =
-  Series $ Vector.create $ do
-    let tLength = Vector.length ts
-    nv <- MVector.new tLength
-    a <- newSTRef @Int 0
-    let writeAtFrom i ref v = do
-          MVector.write nv i $ v
-          modifySTRef ref succ
-    for_ [0 .. tLength - 1] $ \i -> do
-      ai <- readSTRef a
-      let t = ts Vector.! i
-      case findLargestSmallerThan t xs of
-        Nothing -> pure ()
-        Just (DataPoint _ x) -> writeAtFrom ai a $ DataPoint t x
-    ai <- readSTRef a
-    pure $ MVector.slice 0 ai nv
-
--- | Series with no data points.
-emptySeries :: Series a
-emptySeries = Series Vector.empty
+import Data.Series.TimeRange (TimeRange(TimeRange))
 
 -- | Create a series with only a single data point.
 singleton :: UTCTime -> a -> Series a
@@ -84,14 +33,7 @@ size (Series dps) = Vector.length dps
 series :: [(UTCTime, a)] -> Series a
 series = Series . Vector.fromList . sortOn (.time) . fmap (uncurry DataPoint)
 
--- | True if a 'Series' has any elements.
-isEmpty :: Series a -> Bool
-isEmpty (Series dps) = Vector.null dps
-
-{- | Slice a series from one time to another. Inclusive. /O(log n)/.
-
-     /O(log n)/.
--}
+-- | Slice a series from one time to another. Inclusive. /O(log n)/.
 slice :: Show a => UTCTime -> UTCTime -> Series a -> Series a
 slice _ _ s | isEmpty s = emptySeries
 slice start end s@(Series dps) =
@@ -114,37 +56,6 @@ lookup t s = fmap ((.value) . snd) $ exact =<< binarySearch t s
 -}
 (!?) :: forall a. Series a -> UTCTime -> Maybe a
 (!?) = flip Data.Series.lookup
-
-{- | Merge two series, preserving temporal order.
-
-     /O(n+m)/.
--}
-merge :: Series a -> Series a -> Series a
-merge (Series dpsA) (Series dpsB) =
-  Series $ Vector.create $ do
-    let newLength = Vector.length dpsA + Vector.length dpsB
-    nv <- MVector.new newLength
-    a <- newSTRef @Int 0
-    b <- newSTRef @Int 0
-    let writeAtFrom i ref v = do
-          MVector.write nv i $ v
-          modifySTRef ref succ
-
-    for_ [0 .. newLength - 1] $ \i -> do
-      ai <- readSTRef a
-      bi <- readSTRef b
-      case (dpsA Vector.!? ai, dpsB Vector.!? bi) of
-        (Just av, Nothing) -> writeAtFrom i a av
-        (Nothing, Just bv) -> writeAtFrom i b bv
-        (Just av, Just bv) ->
-          case compare av.time bv.time of
-            EQ -> writeAtFrom i a av
-            LT -> writeAtFrom i a av
-            GT -> writeAtFrom i b bv
-        (Nothing, Nothing) ->
-          -- FIXME(Emily, 26 June 2023): This should never happen, but let's be lenient for now.
-          pure ()
-    pure nv
 
 {- | Compute the bounds of a 'Series'.
 
--- src/Data/Series/Internal.hs
+++ src/Data/Series/Internal.hs
@@ -4,12 +4,17 @@
 {- | Perform binary search on a 'Series' for a time.
      This is a low-level operation.
 -}
-module Data.Series.Internal (binarySearch, linearSearch, SearchResult (..), inclusiveSlice, latest, Series (..), DataPoint (..), exact) where
+module Data.Series.Internal (binarySearch, linearSearch, SearchResult (..), inclusiveSlice, latest, Series (..), DataPoint (..), exact, resampleSAH, merge, isEmpty, emptySeries, pairWiseZipWith) where
 
+import Data.Foldable (for_)
+import Data.Maybe (fromJust)
+import Data.STRef (modifySTRef, newSTRef, readSTRef)
 import Data.These (These (..), these)
 import Data.Time (UTCTime)
 import Data.Vector (Vector)
 import Data.Vector qualified as Vector
+import Data.Vector.Mutable qualified as MVector
+import Prelude hiding (lookup)
 
 -- | Represents a data point in a series. It has a time and a value.
 data DataPoint a = DataPoint
@@ -68,6 +73,88 @@ linearSearch t (Series xs) =
     go dp i | i == len - 1 = Nearest (This (i, dp))
     go _ i = go (xs Vector.! (i + 1)) (i + 1)
 
+-- | True if a 'Series' has any elements.
+isEmpty :: Series a -> Bool
+isEmpty (Series dps) = Vector.null dps
+
+-- | Series with no data points.
+emptySeries :: Series a
+emptySeries = Series Vector.empty
+
+findLargestSmallerThan ::
+  forall a.
+  UTCTime ->
+  Series a ->
+  Maybe (DataPoint a)
+findLargestSmallerThan t (Series xs) =
+  lastMaybe $
+    Vector.filter
+      (\(DataPoint t0 _) -> t0 <= t)
+      xs
+  where
+    lastMaybe :: forall b. Vector b -> Maybe b
+    lastMaybe xs' | Vector.null xs' = Nothing
+    lastMaybe xs' = Just $ Vector.last xs'
+
+{- | Merge two series, preserving temporal order.
+
+     /O(n+m)/.
+-}
+merge :: Series a -> Series a -> Series a
+merge (Series dpsA) (Series dpsB) =
+  Series $ Vector.create $ do
+    let newLength = Vector.length dpsA + Vector.length dpsB
+    nv <- MVector.new newLength
+    a <- newSTRef @Int 0
+    b <- newSTRef @Int 0
+    let writeAtFrom i ref v = do
+          MVector.write nv i $ v
+          modifySTRef ref succ
+
+    for_ [0 .. newLength - 1] $ \i -> do
+      ai <- readSTRef a
+      bi <- readSTRef b
+      case (dpsA Vector.!? ai, dpsB Vector.!? bi) of
+        (Just av, Nothing) -> writeAtFrom i a av
+        (Nothing, Just bv) -> writeAtFrom i b bv
+        (Just av, Just bv) ->
+          case compare av.time bv.time of
+            EQ -> writeAtFrom i a av
+            LT -> writeAtFrom i a av
+            GT -> writeAtFrom i b bv
+        (Nothing, Nothing) ->
+          -- FIXME(Emily, 26 June 2023): This should never happen, but let's be lenient for now.
+          pure ()
+    pure nv
+
+{- | Create a new series with the times in the given vector, at each of the
+     times in the vector place the last registered value in the given series
+     before that time.
+
+     If a given time from the vector is before the first time in the series,
+     drop it.
+
+     Passing an empty vector or series will always result in an empty series.
+-}
+resampleSAH :: forall a. Vector UTCTime -> Series a -> Series a
+resampleSAH _ xs | isEmpty xs = emptySeries
+resampleSAH ts xs =
+  Series $ Vector.create $ do
+    let tLength = Vector.length ts
+    nv <- MVector.new tLength
+    a <- newSTRef @Int 0
+    let writeAtFrom i ref v = do
+          MVector.write nv i $ v
+          modifySTRef ref succ
+    for_ [0 .. tLength - 1] $ \i -> do
+      ai <- readSTRef a
+      let t = ts Vector.! i
+      case findLargestSmallerThan t xs of
+        Nothing -> pure ()
+        Just (DataPoint _ x) -> writeAtFrom ai a $ DataPoint t x
+    ai <- readSTRef a
+    pure $ MVector.slice 0 ai nv
+
 {- | Represents the result of performing binary search for a particular time in a 'Series'.
      This API is quite low-level and exposes index information into the Vector.
 -}
@@ -114,3 +201,27 @@ inclusiveSlice lb ub =
   case (inclusiveIndexLB lb, inclusiveIndexUB ub) of
     (Just i, Just j) -> Just (i, j)
     (_, _) -> Nothing
+
+extractTimes :: forall a. Series a -> Vector UTCTime
+extractTimes (Series s) = (.time) <$> s
+
+applySAH :: forall a. Vector UTCTime -> Series a -> Series a
+applySAH ts s = merge s $ resampleSAH ts s
+
+lookup :: UTCTime -> Series a -> Maybe a
+lookup t s = fmap ((.value) . snd) $ exact =<< binarySearch t s
+
+unsafeLookup :: UTCTime -> Series a -> a
+unsafeLookup t s = fromJust $ lookup t s
+
+pairWiseZipWith :: forall a b c. (a -> b -> c) -> Series a -> Series b -> Series c
+pairWiseZipWith f sa sb = Series $ fmap (\dp -> dp {value = f (unsafeLookup dp.time resampledSeriesA) dp.value}) $ deconstr resampledSeriesB
+  where
+    resampledSeriesA :: Series a
+    resampledSeriesA = applySAH (extractTimes sb) sa
+
+    resampledSeriesB :: Series b
+    resampledSeriesB = applySAH (extractTimes sa) sb
+
+    deconstr :: forall d. Series d -> Vector (DataPoint d)
+    deconstr (Series s) = s
